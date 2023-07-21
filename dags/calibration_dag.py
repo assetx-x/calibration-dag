f823f1089dceecc3b509be9e168886eca612e2a9
alexanderjohnson <thealexandercj@gmail.com>
2023-07-21 14:56:24 -0400

    feature(calibration_dag.py) new test dag file

---
+++ dags/calibration_dag.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import sys
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+
+import data_pull_step as dps
+
+""" Calibration Process"""
+
+with DAG(dag_id="taskgroup_example", start_date=days_ago(1)) as dag:
+    start = DummyOperator(task_id="start")
+
+    with TaskGroup("taskgroup_1", tooltip="task group #1") as section_1:
+        task_1 = BashOperator(task_id="op-1", bash_command=":")
+        task_2 = BashOperator(task_id="op-2", bash_command=":")
+
+    with TaskGroup("taskgroup_2", tooltip="task group #2") as section_2:
+        task_3 = BashOperator(task_id="op-3", bash_command=":")
+        task_4 = BashOperator(task_id="op-4", bash_command=":")
+
+    some_other_task = DummyOperator(task_id="some-other-task")
+
+    end = DummyOperator(task_id="end")
+
+    start >> section_1 >> some_other_task >> section_2 >> end
+
+
+
+"""@dag(
+
+    start_date= datetime(2023,7,22),
+    catchup=False,
+    tags=['calibration_test']
+
+)
+def data_pull():
+
+    # DataPull step of dag
+    @task()
+    def S3SecurityMasterReader():
+        data_pull_step.S3SecurityMasterReader().do_step_action()
+
+
+    @task()
+    def S
+"""
+
+
+
+
+#CalculateTargetReturns.do_step_action()
+
