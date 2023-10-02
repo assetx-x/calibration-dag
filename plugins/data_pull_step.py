5a32abe20e2ddf63d98adb2c2b47e75d1a612974
alexanderjohnson <thealexandercj@gmail.com>
2023-10-01 23:31:34 -0400

    fix(calibration_dag.py) starting dag at MergeStep and adding execution_timeout to 150 mins

--- plugins/data_pull_step.py
+++ plugins/data_pull_step.py
@@ -44,6 +44,20 @@ class S3SecurityMasterReader(GCPReader):
     def _post_process_pulled_data(self, data, **kwargs):
         data.drop(data.columns[self.index_col], axis=1, inplace=True)
         data["dcm_security_id"] = data["dcm_security_id"].astype(int)
+        tenere_list = ['NKE', 'PANW', 'BDX', 'EL', 'CP', 'INTC', 'SNOW', 'MSFT', 'AMD',
+                                         'DHR', 'YUM', 'PLD', 'TMUS', 'MDB', 'AMZN', 'BSX', 'SBUX', 'AAPL',
+                                         'NVDA', 'TMO', 'V', 'UNP', 'META', 'UNH', 'GOOGL', 'NOW', 'ISRG',
+                                         'SLB', 'TSM', 'TEAM', 'INTU', 'SPGI', 'CHTR', 'LULU', 'DLTR',
+                                         'NOC', 'BA', 'IDXX', 'DDOG', 'SBAC', 'ZTS', 'LNG', 'WMG', 'BX',
+                                         'MA', 'MCK', 'TTWO', 'HLT', 'FIS', 'VMC', 'JCI', 'ROK', 'DIS',
+                                         'ADI', 'LIN', 'BBWI', 'NET', 'BLK', 'MTCH', 'NFLX', 'ADBE', 'CRM',
+                                         'NYT', 'SQSP', 'DISH', 'SPLK', 'COUP', 'CSCO', 'UBER', 'VMEO',
+                                         'AXP', 'JPM', 'TXN', 'ABNB', 'ULTA', 'TWLO', 'EB', 'GLW', 'TJX',
+                                         'ILMN', 'LYFT', 'MU', 'BKNG', 'VIEW', 'CMCSA', 'OSCR', 'SHOP', 'SPY']
+
+        test_list = list(data['ticker'].unique()[0:50])
+        full_test_universe = list(set(tenere_list + test_list))
+        data = data[data['ticker'].isin(full_test_universe)]
 
         """if self.task_params.run_mode==TaskflowPipelineRunMode.Test:
             query = "select ticker from whitelist where run_dt in ('{0}') and as_of_end is null"\
