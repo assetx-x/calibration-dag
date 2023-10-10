fc525690eeffbe6ee1de500803fb5d0cf16b35dd
henriquelobato <iklobato1@gmail.com>
2023-10-10 15:19:10 -0300

    fix(DAG): add default parameters on daily equity prices update dag

--- dags/daily_equity_price_update.py
+++ dags/daily_equity_price_update.py
@@ -1,10 +1,20 @@
+from datetime import datetime
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
 
 from insert_data import main
 
-with DAG(dag_id="daily_equity_prices_auto_updater", start_date=days_ago(1)) as dag:
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'schedule_interval': '0 1 * * *',  # Schedule to everyday at 1AM
+}
+
+
+with DAG(dag_id="daily_equity_prices_auto_updater", default_args=default_args) as dag:
 
     tart_task = PythonOperator(
         task_id="update_daily_equity_prices_main",
