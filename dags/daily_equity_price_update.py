0351d6a018b5707323aa29cf004c0c1a3fad82b9
henriquelobato <iklobato1@gmail.com>
2023-10-10 15:30:41 -0300

    fix(DAG): add missing start_date parameter

--- dags/daily_equity_price_update.py
+++ dags/daily_equity_price_update.py
@@ -10,6 +10,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'retries': 1,
+    'start_date': datetime(2023, 1, 1),
     'schedule_interval': '0 1 * * *',  # Schedule to everyday at 1AM
 }
 
@@ -21,3 +22,6 @@ with DAG(dag_id="daily_equity_prices_auto_updater", default_args=default_args) a
         python_callable=main,
     )
 
+
+if __name__ == '__main__':
+    dag.test()
