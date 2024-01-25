4e8b7743aec59f3f638d09d4f7a3b4229860b65b
henrique <iklobato1@gmail.com>
2024-01-24 23:15:56 -0300

    Refactor ExtractGANFactors class and improve code formatting
    Code formatting in the ExtractGANFactors class has been improved for better readability. Refactor includes cleaning up white spaces, reorganizing import statements, and streamlined dictionary declaration. A new 'airflow.decorators' import has been added, and an 'external_python' task decorator has been used for the 'do_step_action' method. The DataReaderClass inheritance has been enhanced by including 'bucket' and 'key' in the initialization.

--- plugins/extract_gan_factors_step.py
+++ plugins/extract_gan_factors_step.py
@@ -1,41 +1,63 @@
-from core_classes import GCPReader,download_yahoo_data,DataReaderClass
+import json
+
+from airflow.decorators import task
+
+from core_classes import DataReaderClass
 from asset_pricing_model import extract_factors
 import pandas as pd
-import io
 import os
-from pandas_datareader import data as web
-import yfinance as yf
-#import fix_yahoo_finance as fyf
-import sys
-from fredapi import Fred
-import pandas
-from google.cloud import bigquery
-import gc
-import numpy as np
 from datetime import datetime
-from core_classes import construct_required_path,construct_destination_path,convert_date_to_string
-
+from core_classes import (
+    construct_required_path,
+    construct_destination_path,
+    convert_date_to_string,
+)
 
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 plugins_folder = os.path.join(parent_directory, "plugins")
 data_processing_folder = os.path.join(plugins_folder, "data_processing")
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(data_processing_folder,'dcm-prod.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(
+    data_processing_folder, 'dcm-prod.json'
+)
 os.environ['GCS_BUCKET'] = 'dcm-prod-ba2f-us-dcm-data-test'
-JUMP_DATES_CSV = os.path.join(data_processing_folder,'intervals_for_jump.csv')
+JUMP_DATES_CSV = os.path.join(data_processing_folder, 'intervals_for_jump.csv')
 current_date = datetime.now().date()
 RUN_DATE = current_date.strftime('%Y-%m-%d')
 
-class ExtractGANFactors(DataReaderClass):
 
-    def __init__(self, insample_cut_date, epochs, epochs_discriminator,gan_iterations,
-                 retrain):
+class ExtractGANFactors(DataReaderClass):
+    def __init__(
+        self,
+        insample_cut_date,
+        epochs,
+        epochs_discriminator,
+        gan_iterations,
+        retrain,
+        bucket,
+        key,
+    ):
+        super().__init__(bucket, key)
         self.insample_cut_date = insample_cut_date
         self.epochs = epochs
         self.epochs_discriminator = epochs_discriminator
         self.gan_iterations = gan_iterations
         self.retrain = retrain
+        self._data = None
 
+    @property
+    def data(self):
+        print(f'[>] Extracting data from {self.key}')
+        return self._data
 
+    @data.setter
+    def data(self, val):
+        print(f'[>] Setting data to {val}!')
+        self._data = val
+
+    @task.external_python(
+        task_id="external_python",
+        python='/home/dcmadmin/calibration_ml_training/bin/python',
+    )
     def do_step_action(self, **kwargs):
         full_path = 'gs://dcm-prod-ba2f-us-dcm-data-test/calibration_data/live/save_gan_inputs/save_gan_inputs'
         factor_file = os.path.join(full_path, "all_factors.h5")
@@ -48,22 +70,20 @@ class ExtractGANFactors(DataReaderClass):
         return {'gan_factors': self.data}
 
 
+extract_gan_factors_params = {
+    'insample_cut_date': "2023-02-03",
+    'epochs': 150,
+    'epochs_discriminator': 50,
+    'gan_iterations': 2,
+    'retrain': False,
+}
 
-
-
-extract_gan_factors_params = {'insample_cut_date': "2023-02-03",
-                    'epochs': 150,
-                    'epochs_discriminator': 50,
-                    'gan_iterations': 2,
-                    'retrain': False,
-                }
-
-
-
-ExtractGANFactors_params = {'params':extract_gan_factors_params,
-                                   'class':ExtractGANFactors,
-                                       'start_date':RUN_DATE,
-                                'provided_data': {'gan_factors': construct_destination_path('generate_gan_results'),
-                                                 },
-                                'required_data': {}}
-
+ExtractGANFactors_params = {
+    'params': extract_gan_factors_params,
+    'class': ExtractGANFactors,
+    'start_date': RUN_DATE,
+    'provided_data': {
+        'gan_factors': construct_destination_path('generate_gan_results'),
+    },
+    'required_data': {},
+}
