99de57c79049107e21ee4bc3daa0d3ec2c82fe85
henrique <iklobato1@gmail.com>
2024-01-26 20:03:09 -0300

    fix(dag): send asset_pricing_model_gan

--- src/generate_gan_results_step.py
+++ src/generate_gan_results_step.py
@@ -1,11 +1,7 @@
 from asset_pricing_model import extract_factors
-from core_classes import DataReaderClass
 import pandas as pd
 import os
 from datetime import datetime
-from core_classes import (
-    construct_destination_path,
-)
 
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 plugins_folder = os.path.join(parent_directory, "plugins")
@@ -23,7 +19,7 @@ RUN_DATE = current_date.strftime('%Y-%m-%d')
 #    task_id="external_python",
 #    python='/home/dcmadmin/calibration_ml_training/bin/python',)
 
-class ExtractGANFactors(DataReaderClass):
+class ExtractGANFactors:
 
     def __init__(self, insample_cut_date, epochs, epochs_discriminator,gan_iterations,
                  retrain):
@@ -39,7 +35,6 @@ class ExtractGANFactors(DataReaderClass):
     def _prepare_to_pull_data(self):
         pass
 
-
     def do_step_action(self, **kwargs):
         full_path = 'gs://dcm-prod-ba2f-us-dcm-data-test/calibration_data/live/save_gan_inputs/save_gan_inputs'
         factor_file = os.path.join(full_path, "all_factors.h5")
@@ -53,7 +48,7 @@ class ExtractGANFactors(DataReaderClass):
 
 
 extract_gan_factors_params = {
-    'data_dir':'gs://dcm-prod-ba2f-us-dcm-data-test/calibration_data/local/save_gan_inputs',
+    'data_dir': 'gs://dcm-prod-ba2f-us-dcm-data-test/calibration_data/local/save_gan_inputs',
     'insample_cut_date': "2023-02-03",
     'epochs': 150,
     'epochs_discriminator': 50,
@@ -66,7 +61,7 @@ ExtractGANFactors_params = {
     'class': ExtractGANFactors,
     'start_date': RUN_DATE,
     'provided_data': {
-        'gan_factors': construct_destination_path('generate_gan_results'),
+        'gan_factors': "gs://{}/calibration_data/live" + "/{}/".format('generate_gan_result') + "{}.csv"
     },
     'required_data': {},
 }
