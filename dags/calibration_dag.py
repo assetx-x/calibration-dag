f29649c6d6d7418cd3c4c969f1855641a157b67e
henrique <iklobato1@gmail.com>
2024-01-27 00:46:23 -0300

    Update python path in calibration script
    The python path used in 'calibration_dag.py' has been updated to point to a specific version of python. This refactoring ensures that the script runs with a predetermined and tested Python version, guaranteeing compatibility and stability.

--- dags/calibration_dag.py
+++ dags/calibration_dag.py
@@ -110,7 +110,7 @@ with DAG(dag_id="calibration", start_date=days_ago(1)) as dag:
     ) as GenerateGANResults:
 
         @task.external_python(
-            task_id="external_python", python='/home/airflow/.pyenv/shims/python3.6'
+            task_id="external_python", python='//home/airflow/.pyenv/versions/3.6.10/bin/python'
         )
         def callable_external_python():
 
