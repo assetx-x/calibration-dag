6ac247062bea8cc893ba24053426ae99bca9a831
henrique <iklobato1@gmail.com>
2024-01-28 03:50:11 -0300

    log(asset_pricing_model): print data directory

--- plugins/asset_pricing_model.py
+++ plugins/asset_pricing_model.py
@@ -141,7 +141,9 @@ def delete_directory_content(directory, del_subdirs=True, delete_root_dir=False)
 
 def load_data(data_dir):
     print("loading data with shapes.....")
-    df = pd.read_csv(os.path.join(data_dir, "company_data.csv"), index_col=0).fillna(0.0)
+    data_dir = os.path.join(data_dir, "company_data.csv")
+    print(f'Data directory > {data_dir}')
+    df = pd.read_csv(data_dir, index_col=0).fillna(0.0)
     econ_data = pd.read_csv(os.path.join(data_dir, "econ_data_final.csv"), index_col=0)
     active_matrix = pd.read_csv(os.path.join(data_dir, "active_matrix.csv"), index_col=0).fillna(0)
     return_data = pd.read_csv(os.path.join(data_dir, "future_returns.csv"), index_col=0).fillna(0.0)
