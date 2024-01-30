6772531e9ade3c02e5a7ec3e4dd5473841cc7753
henrique <iklobato1@gmail.com>
2024-01-30 17:43:06 -0300

    Add strategy_prediction_calculation_dag_sensor file
    A new Python file named strategy_prediction_calculation_dag_sensor has been added. It contains a definition for a new DAG with 'your_dag' as its ID, default arguments, and a task for creating Google Cloud Storage(object existence) sensors for files 'growth_predictions.csv' and 'largecap_growth_predictions.csv'. This addition appears to be aimed at regularly checking existence of certain objects in Google Cloud Storage.

---
+++ dags/strategy_prediction_calculation_dag_sensor.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+
+from dags.strategy_prediction_calculation_dag import list_files_task, GS_BUCKET_NAME
+
+
+STRATEGY_PATH_BLOB = 'api_v2_storage/strategies/*'
+INACTIVITY_PERIOD = 30
+MIN_OBJECT_AGE = 300
+
+
+default_args = {
+    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'retries_delay': timedelta(minutes=1)
+}
+
+dag = DAG('your_dag', default_args=default_args, schedule_interval=timedelta(minutes=5))
+
+
+def create_gcs_file_sensor(obj):
+    task_id = 'gcs_file_sensor_' + obj.split(".")[0]
+    return GCSObjectExistenceSensor(
+        task_id=task_id,
+        bucket=GS_BUCKET_NAME,
+        object=obj,
+        inactivity_period=INACTIVITY_PERIOD,
+        min_object_age=MIN_OBJECT_AGE,
+        allow_delete=False,
+        blob=STRATEGY_PATH_BLOB,
+        dag=dag
+    )
+
+[
+    create_gcs_file_sensor('growth_predictions.csv'),
+    create_gcs_file_sensor('largecap_growth_predictions.csv')
+] >> list_files_task
