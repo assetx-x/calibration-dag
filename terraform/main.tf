4d20e426f0b64069f67d259c3914368086af27bc
henrique <iklobato1@gmail.com>
2024-09-02 14:32:27 -0300

    infra(vm): refac

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,10 @@ resource "google_compute_instance" "calibration-ax" {
   machine_type = "e2-highmem-8"
 
   metadata = {
-    ssh-keys = "iklo:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyPnxThY1CZf9vqv9i9WlIumqBAuOIo/1UxwrEvSXxNHgoTKXzaLCQfw3JWtApdnTqo0GRvPcVby44DJUlBEywwraWgUo317VSyFP7CDkz2zvG07NWTYLK4COQPejFqFLNFfHTpIOAEz1WYpCwlXOYtUQk3BGORZ+adKzxbt/tFYYC8ymbgQz8MjooUQQiKt/YFejqw+i2vmbl9mnTmRZoDSdeK3EhLWlLXHbNggjlR9eu17puo3V7UO9c4SqaJuBzoSX9Mfaq9SuZtjAld93U6WCGuiUlqDcJc7Bdu+mjAURmWx8hWj9muPlAAg+mEdOzkHrvk49ztuRke+IeDoxRvBYkjTCRlV8tChYRdwP3m1QgUHX79kP2IJNdQ9KNC6p+2z/AlLRWqhbgJ99qZqbA5f2FSUe5nHuv+9Jd0oqprbDN0JXR0UGn0YNN9FQzN0WrpCtLEEGPTcRCi46Asbtr6AKMFnIzLU5lxubvCPYT59Zkt8suBO1d4znachMX14s= iklo@iklos-MacBook-Pro.local"
+    ssh-keys = <<-EOT
+      iklo:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyPnxThY1CZf9vqv9i9WlIumqBAuOIo/1UxwrEvSXxNHgoTKXzaLCQfw3JWtApdnTqo0GRvPcVby44DJUlBEywwraWgUo317VSyFP7CDkz2zvG07NWTYLK4COQPejFqFLNFfHTpIOAEz1WYpCwlXOYtUQk3BGORZ+adKzxbt/tFYYC8ymbgQz8MjooUQQiKt/YFejqw+i2vmbl9mnTmRZoDSdeK3EhLWlLXHbNggjlR9eu17puo3V7UO9c4SqaJuBzoSX9Mfaq9SuZtjAld93U6WCGuiUlqDcJc7Bdu+mjAURmWx8hWj9muPlAAg+mEdOzkHrvk49ztuRke+IeDoxRvBYkjTCRlV8tChYRdwP3m1QgUHX79kP2IJNdQ9KNC6p+2z/AlLRWqhbgJ99qZqbA5f2FSUe5nHuv+9Jd0oqprbDN0JXR0UGn0YNN9FQzN0WrpCtLEEGPTcRCi46Asbtr6AKMFnIzLU5lxubvCPYT59Zkt8suBO1d4znachMX14s= iklo@iklos-MacBook-Pro.local
+      alexanderjohnson:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCxOFiDDYoHUuN7+HZbL8sVfIzAC99Yhf3Q40L6+tJZLsmgf8gLscRaRRtyJ315ZLI17W7Bm98bFsNcRup35/rR6zDZ9Wk11OU9nMtCp4fpLpzp/J+wfDFgjvYZIaWpf/FXEga95bXCd1z4HOBSzVy5kuNbRENfm0P5Flp1sGVQwy8ywclDG80y0Toa/DOnFG1oab66k+hEeFsf59G1TDuiFQINm+ZbROdcmS9rmSNigLTh1itlaLah3HkRN/v84JRWxc9T8MxLymSG2Xk75pSO492C13A/w7x/cQJsyH6Jj6U7mVpHB9Qwastjm/wSCZGmX+aHLoLvUK597ssUn0LgeFMkLbbguqag4/O7Ma8fM2EeaZsSS8jBcS/VS9D2uPZD7ajuPSOvUyXLya72mLmCNLAW70Dvj/rWlZh53aqnq/mYdA+mpOYfVqaCKDA9rLxPlF0XYjE5vAAZPNr9kePKwkdOxDYM//Fsat7mE3mdFSs46CBZrYxE4kMdc9SXBYM= alexanderjohnson@Alexanders-MBP.lan
+    EOT
 
     startup-script = <<-EOT
       #!/bin/bash
@@ -45,9 +48,6 @@ resource "google_compute_instance" "calibration-ax" {
       sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
       sudo groupadd docker
       sudo usermod -aG docker $USER
-
-      echo "SSH Public Key:"
-      cat ~/.ssh/id_rsa.pub
     EOT
   }
 
@@ -72,7 +72,14 @@ resource "google_compute_instance" "calibration-ax" {
 
   service_account {
     email  = "1058145182637-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
   }
 
   shielded_instance_config {
